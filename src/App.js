de27aa7d3d30852503e50ba59c585f98eac56d6c
YuvalYacoby <yuvaly@LAPTOP-RR6IHMO0.localdomain>
2020-06-04 13:05:28 +0300

    end of phase one

--- src/App.js
+++ src/App.js
@@ -1,13 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
-import BingMap from './componenets/BingMap';
-import MapForm from './componenets/MapForm';
+import { ReactBingmaps } from 'react-bingmaps';
 
 const App = () => {
+  const [coords, setCoords] = useState([]);
+
+  const AddPushPinOnClick = location => {
+    const lat = location.latitude;
+    const lon = location.longitude;
+
+    setCoords(coords.push([lat, lon]));
+  };
+
   return (
     <div className='wrapper'>
-      <MapForm />
-      <BingMap />
+      <div className='form-wrapper'>
+        {' '}
+        <form className='form'>
+          <div className='form-group'>
+            <label htmlFor='coords'>Add Coords</label>
+            <input
+              className='form-control'
+              type='text'
+              placeholder='Set lat coords'
+              name='lat'
+              required
+            />
+            <input
+              className='form-control'
+              type='text'
+              placeholder='Set lon coords'
+              name='lon'
+              required
+            />
+          </div>
+          <input type='submit' className='btn btn-primary' value='Submit' />
+        </form>
+      </div>
+      <div className='map-wrapper'>
+        <ReactBingmaps
+          bingmapKey='Ajpth3bKmJjnCjywlZfR5joLB8wnJnfHn7Qu_N0UHKNc-lR3Q7eOGUpGjIfB6VHP'
+          center={[32.109, 34.855]}
+          getLocation={{ addHandler: 'click', callback: AddPushPinOnClick }}
+          polyline={{
+            location: [{ coords }],
+            option: {
+              strokeColor: 'red',
+              strokeThickness: 2,
+              strokeDashArray: [1, 2, 5, 10]
+            }
+          }}
+        ></ReactBingmaps>
+      </div>
     </div>
   );
 };
