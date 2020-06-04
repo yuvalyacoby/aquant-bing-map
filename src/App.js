5832372c534ebc7a905ccbfe7a211114078cd794
YuvalYacoby <yuvaly@LAPTOP-RR6IHMO0.localdomain>
2020-06-04 12:09:53 +0300

    initial layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import './App.css';
+import BingMap from './componenets/BingMap';
+import MapForm from './componenets/MapForm';
 
 const App = () => {
-  return <>Home</>;
+  return (
+    <div className='wrapper'>
+      <MapForm />
+      <BingMap />
+    </div>
+  );
 };
 
 export default App;
