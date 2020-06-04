d800e1c294c203a28fd4ac2f6b47559316146c65
YuvalYacoby <yuvaly@LAPTOP-RR6IHMO0.localdomain>
2020-06-04 13:46:31 +0300

    implementation concept, not working

--- src/App.js
+++ src/App.js
@@ -4,34 +4,58 @@ import { ReactBingmaps } from 'react-bingmaps';
 
 const App = () => {
   const [coords, setCoords] = useState([]);
+  const [formData, setFormData] = useState({
+    lat: null,
+    lon: null
+  });
+  const [pins, setPins] = useState([]);
 
   const AddPushPinOnClick = location => {
     const lat = location.latitude;
     const lon = location.longitude;
 
-    setCoords(coords.push([lat, lon]));
+    setPins([
+      {
+        location: [lat, lon],
+        option: { color: 'red' }
+      }
+    ]);
+
+    setCoords([lat, lon]);
+  };
+
+  const onChange = e =>
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+
+  const onSubmit = e => {
+    e.preventDefault();
+    setCoords(coords);
   };
 
   return (
     <div className='wrapper'>
       <div className='form-wrapper'>
         {' '}
-        <form className='form'>
+        <form className='form' onSubmit={e => onSubmit(e)}>
           <div className='form-group'>
             <label htmlFor='coords'>Add Coords</label>
             <input
+              id='lat'
               className='form-control'
               type='text'
               placeholder='Set lat coords'
               name='lat'
               required
+              onChange={e => onChange(e)}
             />
             <input
+              id='lon'
               className='form-control'
               type='text'
               placeholder='Set lon coords'
               name='lon'
               required
+              onChange={e => onChange(e)}
             />
           </div>
           <input type='submit' className='btn btn-primary' value='Submit' />
@@ -42,6 +66,7 @@ const App = () => {
           bingmapKey='Ajpth3bKmJjnCjywlZfR5joLB8wnJnfHn7Qu_N0UHKNc-lR3Q7eOGUpGjIfB6VHP'
           center={[32.109, 34.855]}
           getLocation={{ addHandler: 'click', callback: AddPushPinOnClick }}
+          pushPins={{ pins }}
           polyline={{
             location: [{ coords }],
             option: {
@@ -50,6 +75,16 @@ const App = () => {
               strokeDashArray: [1, 2, 5, 10]
             }
           }}
+          regularPolygons={[
+            {
+              location: [
+                [32.109, 34.855],
+                [32.183416374705835, 35.015278620157716],
+                [32.279834579829654, 35.049607563935]
+              ],
+              option: { fillColor: 'green', strokeThickness: 2 }
+            }
+          ]}
         ></ReactBingmaps>
       </div>
     </div>
